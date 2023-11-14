a9f2eadb85b65ed0679942197ae31dcfdb0039c4
rachelviolet <132365064+rachelviolet@users.noreply.github.com>
2023-11-14 20:06:52 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var heroiNome = "Miguel"
+var heroiXP = 1987
+
+if (heroiXP <= 1000) {console.log("O herói de nome " + heroiNome + " está no nível: Ferro")}
+if (heroiXP >= 1001 && heroiXP <= 2000) {console.log("O herói de nome " + heroiNome + " está no nível: Bronze")}
+if (heroiXP >= 2001 && heroiXP <= 5000) {console.log("O herói de nome " + heroiNome + " está no nível: Prata")}
+if (heroiXP >= 6001 && heroiXP <= 7000) {console.log("O herói de nome " + heroiNome + " está no nível: Ouro")}
+if (heroiXP >= 7001 && heroiXP <= 8000) {console.log("O herói de nome " + heroiNome + " está no nível: Platina")}
+if (heroiXP >= 8001 && heroiXP <= 9000) {console.log("O herói de nome " + heroiNome + " está no nível: Ascendente")}
+if (heroiXP >= 9001 && heroiXP <= 10000) {console.log("O herói de nome " + heroiNome + " está no nível: Imortal")}
+if (heroiXP >= 10001) {console.log("O herói de nome " + heroiNome + " está no nível: Radiante")}
